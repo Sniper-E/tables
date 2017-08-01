260021566d7a4004f2ca16c103cd3ee40696fb42
Sniper-E <ehumphrey57@gmail.com>
2017-07-31 22:25:49 -0500

    Adding permissions & Expanding to 7 columns

---
+++ language/en/permissions_tables.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Extension - Tables
+* @copyright (c) 2017 Sniper_E (http://sniper-e.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_U_USE_TABLES_BBCODE'		=> 'Can use tables bbcode',
+));
