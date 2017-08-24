c83696a41903f8839a61cdead3eefcbfec2b5746
Sniper-E <ehumphrey57@gmail.com>
2017-08-24 11:42:26 -0500

    Add Dutch language by Solidjeuh

---
+++ language/nl/permissions_tables.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* @package phpBB Extension - Tables
+* @copyright (c) 2017 Sniper_E (http://sniper-e.com)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+* @Dutch translation by Solidjeuh (https://www.froddelpower.be)
+* Nederlandse vertaling @ Solidjeuh
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACL_U_USE_TABLES_BBCODE'		=> 'Kan tabellen BBcode gebruiken',
+));
